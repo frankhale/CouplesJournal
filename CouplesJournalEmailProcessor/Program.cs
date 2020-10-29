253ba3163b176e4d7383d3016bdec5a145f387af
Frank Hale <frankhale@gmail.com>
2020-10-28 22:02:11 -0400

    Add more error checking

--- CouplesJournalEmailProcessor/Program.cs
+++ CouplesJournalEmailProcessor/Program.cs
@@ -14,7 +14,26 @@ namespace CouplesJournal.EmailProcessor
             var userName = Environment.GetEnvironmentVariable("COUPLES_JOURNAL_SMTP_USERNAME");
             var password = Environment.GetEnvironmentVariable("COUPLES_JOURNAL_SMTP_PASSWORD");
             var host = Environment.GetEnvironmentVariable("COUPLES_JOURNAL_SMTP_SERVER");
-            var port = Int32.Parse(Environment.GetEnvironmentVariable("COUPLES_JOURNAL_SMTP_PORT"));
+            var connString = Environment.GetEnvironmentVariable("COUPLES_JOURNAL_CONNECTION_STRING");
+            int port = 0;
+
+            try
+            {
+                port = Int32.Parse(Environment.GetEnvironmentVariable("COUPLES_JOURNAL_SMTP_PORT"));
+            }
+            catch
+            {
+                Console.WriteLine("Please check the environment variable COUPLES_JOURNAL_SMTP_PORT as this value could not be converted to an integer.");
+            }                      
+
+            if(string.IsNullOrEmpty(userName) ||
+                string.IsNullOrEmpty(password) ||
+                string.IsNullOrEmpty(host) ||                
+                string.IsNullOrEmpty(connString))
+            {
+                Console.WriteLine("The following environment variables need to be set:\n\nCOUPLES_JOURNAL_SMTP_USERNAME\nCOUPLES_JOURNAL_SMTP_PASSWORD\nCOUPLES_JOURNAL_SMTP_SERVER\nCOUPLES_JOURNAL_CONNECTION_STRING");
+                Environment.Exit(-1);
+            }
 
             var mailService = new MailService(new MailSettings
             {
@@ -27,7 +46,7 @@ namespace CouplesJournal.EmailProcessor
                 FromAddress = Environment.GetEnvironmentVariable("COUPLES_JOURNAL_SMTP_USERNAME")
             });
 
-            var api = new CouplesJournalDataApi(new CouplesJournalDbContext(Environment.GetEnvironmentVariable("COUPLES_JOURNAL_CONNECTION_STRING")));
+            var api = new CouplesJournalDataApi(new CouplesJournalDbContext(connString));
             var emailNotifications = await api.GetEmailNotificationsToProcess();
             var emailNotificationsToProcess = emailNotifications.Select(x =>
                 mailService.SendEmailAsync(new MailRequest
