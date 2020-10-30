f27541435e62bb8304a2dcea3bcb6f337e593bad
Frank Hale <frankhale@gmail.com>
2020-10-29 23:20:21 -0400

    Add username, display name to register account page

--- CouplesJournal.Blazor/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ CouplesJournal.Blazor/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -46,6 +46,12 @@ namespace CouplesJournal.Blazor.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
+            public string UserName { get; set; }
+
+            [Required]
+            public string DisplayName { get; set; }
+
+            [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
@@ -74,7 +80,12 @@ namespace CouplesJournal.Blazor.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser 
+                { 
+                    UserName = Input.UserName, 
+                    DisplayName = Input.DisplayName,
+                    Email = Input.Email
+                };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
